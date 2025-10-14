6082d227355ee8be13a9bd71edec4276d76ac208
vini13henrique <vinicius13henrique01@gmail.com>
2025-10-14 00:50:13 -0300

    Service layer, component registration

--- src/main/java/com/vinicius/projeto/repositories/UserRepository.java
+++ src/main/java/com/vinicius/projeto/repositories/UserRepository.java
@@ -1,8 +1,8 @@
 package com.vinicius.projeto.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.vinicius.projeto.entities.User;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
